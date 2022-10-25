78ffb33d4ad6f10f6841d5d47d9b550ff11eaf2d
Caspian Ulvmåne <cabbeulv@gmail.com>
2022-10-25 10:52:46 +0200

    Added pictures and character info

---
+++ PHP_Basics/p1_info/p1_char.php
@@ -0,0 +1,37 @@
+<?php
+    $p1chars = [
+        ["name" => 'Razputin "Raz" Aquato',
+         "info" => "Razputin was born and raised in the Aquato family circus,All his life, Raz's father would tell him and his siblings about a bloody past feud wherein a family of fortune tellers from a rival circus drowned his father and cursed every current and future member of Raz's family to die in water.",
+         "camper",
+         "image" => "Raz.png"
+        ],
+        
+        ["name" => 'Lili Zanotto',
+         "info" => "She's over camp, over the Psychonauts, over the whole thing. Or, at least she was until the day Raz came to camp. Now she finds herself wrapped up in his excitement.",
+         "camper",
+         "image" => "Lilipnror.webp"
+        ],
+        
+        ["name" => 'Dogen Boole',
+         "info" => 'Dogen Boole is a nine year old psychic camper at Whispering Rock Psychic Summer Camp. Due to his difficulty controlling his lethal abilities, his parents have him wear a "special hat" which allegedly keeps his powers under control.',
+         "camper",
+         "image" => "Dogenpng.webp"
+        ],
+        
+        ["name" => 'Robert "Bobby" Zilch',
+         "info" => "Bobby Zilch is aggressive and enjoys putting others down to make himself feel better. Although he puts up a tough persona, he has a softer side.",
+         "camper",
+         "image" => "Bobbypn2.webp"
+        ],
+        
+        ["name" => 'Camilla "Milla" Vodello',
+         "info" => "Milla has a soft and caring demeanor with an upbeat attitude. She is very kind toward her colleagues, pupils, and even her enemies. She is a mother figure at Whispering Rock, and always does her best to keep the campers safe.",
+         "image" => "Milla.png"
+        ],
+        
+        ["name" => 'Sasha Nein',
+         "info" => "Sasha is an intelligent, down to earth, curious scientist interested in studying the full potential of the human mind. To Sasha, being a Psychonaut means exploring the mysterious universe of the human mind.",
+         "image" => "Sasha_nein.png"
+        ],
+    ];
+?>
---
+++ PHP_Basics/p2_info/p2_char.php
@@ -0,0 +1,34 @@
+<?php
+    $p2chars = [
+        ["name" => 'Ford Cruller',
+         "info" => "Ford Cruller is the leader and founder of the Psychic Six and a current Psychonaut agent. He used to be one of the most powerful psychics of his time, but because of his advanced age and shattered mind he was limited to the role of a dispatcher.",
+         "image" => "Fordnormal.png"
+        ],
+        
+        ["name" => 'Robert "Bob" Zanotto',
+         "info" => "Bob Zanotto is a member of the Psychic Six, and Psychonaut, who as a member of the Zanotto family, specializes in herbaphony and Psychic botany. In the past, he was fired by his nephew Truman Zanotto because the trauma he experienced in the Battle of Grulovia made him a danger to his co-workers.",
+         "image" => "Bobzanottopng.webp"
+        ],
+        
+        ["name" => 'Hollis Forsythe',
+         "info" => `Hollis Forsythe is the Second Head of the Psychonauts (formally the "Lesser Head", though she isn't fond of this term), taking over whenever Truman Zanotto is unavailable or unable to take the lead.`,
+         "image" => "Hollisrender.webp"
+        ],
+        
+        ["name" => 'Augustus Aquato',
+         "info" => "Augustus Aquato is a circus acrobat and the father of the Aquato children. He is husband to Donatella Aquato. Despite being a psychic himself, past trauma had caused him to be wary of other psychics.",
+         "image" => "Dadrender2.webp"
+        ],
+        
+        ["name" => 'Samantha "Sam" Boole',
+        "info" => `Samantha[1] "Sam" Boole is a Junior Psychonaut and former Psychonaut intern stationed at the Motherlobe. She is Dogen's older sister and Compton's granddaughter.`,
+        "intern",
+         "image" => "Samrender.webp"
+        ],
+        
+        ["name" => 'Gisu Nerumen',
+         "info" => "Gisu is an engineering expert who is very good at skateboarding. She is energetic, high energy, and has a bright personality. She is passionate about inventing gadgets with her mentor.",
+         "image" => "Gisurender.webp"
+        ]
+    ];
+?>
