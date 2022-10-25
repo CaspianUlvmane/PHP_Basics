b7e164dc08ea2ab836a5aa280b74917f3a70f15e
Caspian Ulvmåne <cabbeulv@gmail.com>
2022-10-25 11:33:36 +0200

    render characters

--- PHP_Basics/p1.php
+++ PHP_Basics/p1.php
@@ -13,11 +13,30 @@
     echo "<p>$p1_info</p>";
     require_once"./p1_info/p1_chars.php";
     ?>
-    <pre>
+
     <?php
-        var_dump($p1chars);
+        foreach($p1chars as $char){
+            echo "<div class='char'>";
+            if(array_key_exists("camper", $char)){
+                $name = $char["name"];
+                $info = $char["info"];
+                $url = $char["image"];
+                echo "<img src='./Char_img/$url' alt='$name'></img>";
+                echo "<h2>$name</h2>";
+                echo "<p>$info</p>";
+                echo "<p>Camper at Whispering Rock</p>";
+            } else {
+                $name = $char["name"];
+                $info = $char["info"];
+                $url = $char["image"];
+                echo "<img src='./Char_img/$url' alt='$name'></img>";
+                echo "<h2>$name</h2>";
+                echo "<p>$info</p>";
+            };
+
+            echo "</div>";
+        };
     ?>
-    </pre>
 
 <?php
  require_once"./core/footer.php";
